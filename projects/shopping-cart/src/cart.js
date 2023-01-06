937487387218ea3c9e4303f2a9e7f74af34c7ebb
Нуртилек Кошкар <nurtylek98@gmail.com>
2023-01-06 13:01:35 +0600

    cart js

--- projects/shopping-cart/src/cart.js
+++ projects/shopping-cart/src/cart.js
@@ -2,53 +2,59 @@ const shoppingForm = document.querySelector(".shopping");
 const list = document.querySelector(".list");
 // const buttons = list.querySelectorAll('button');
 
-const itemsUpdated = 'itemsUpdated';
-const itemsKey = 'items'
-
+// State
 let items = [];
+
+const LS_KEYS = {
+    ITEMS: 'items'
+}
+
+const bc = new BroadcastChannel("test_channel");
 
 function displayItems() {
     const html = items.map(item => `
-        <li>
-            <input type="checkbox" id="${item.id}" ${item.isCompleted && 'checked'}/>
-            <span>${item.name}</span>
-            <button value="${item.id}">&times;</button>
-        </li>
-    `).join('');
+    <li>
+        <input type="checkbox" id="${item.id}" ${item.isCompleted ? 'checked': ''}>
+        <span>${item.name}</span>
+        <button id="${item.id}">&times;</button>
+    </li>
+`).join('');
     list.innerHTML = html;
 }
 
 function setDataToLocalStorage() {
-    localStorage.setItem(itemsKey, JSON.stringify(items));
+    localStorage.setItem(LS_KEYS.ITEMS, JSON.stringify(items));
 }
 
 function restoreDataFromLocalStorage() {
-    const storedItems = JSON.parse(localStorage.getItem(itemsKey));
-    if (storedItems?.length) {
-        items.push(...storedItems);
-        list.dispatchEvent(new CustomEvent(itemsUpdated));
+    const data = JSON.parse(localStorage.getItem(LS_KEYS.ITEMS));
+    if (data?.length) {
+        items.push(...data)
+        list.dispatchEvent(new CustomEvent('itemsUpdated'));
     }
 }
 
 function handleSubmit(event) {
     event.preventDefault();
-    const name = event.currentTarget.item.value;
+    const name = event.target.item.value;
 
     if (!name) return;
 
     const item = {
-        name,
+        name: name,
         isCompleted: false,
         id: crypto.randomUUID()
-    };
+    }
+
     items.push(item);
-    event.target.reset();
-    list.dispatchEvent(new CustomEvent(itemsUpdated));
+    bc.postMessage(items)
+    event.currentTarget.reset();
+    list.dispatchEvent(new CustomEvent('itemsUpdated'));
 }
 
 function deleteItem(id) {
     items = items.filter(item => item.id !== id);
-    list.dispatchEvent(new CustomEvent(itemsUpdated));
+    list.dispatchEvent(new CustomEvent('itemsUpdated'));
 }
 
 function markAsCompleted(id) {
@@ -56,26 +62,32 @@ function markAsCompleted(id) {
         ...item,
         isCompleted: !item.isCompleted
     }: item);
-    list.dispatchEvent(new CustomEvent(itemsUpdated));
+    list.dispatchEvent(new CustomEvent('itemsUpdated'));
 }
 
 shoppingForm.addEventListener('submit', handleSubmit);
-list.addEventListener(itemsUpdated, displayItems);
-list.addEventListener(itemsUpdated, setDataToLocalStorage);
-
 window.addEventListener('load', restoreDataFromLocalStorage);
+list.addEventListener('itemsUpdated', displayItems);
+list.addEventListener('itemsUpdated', setDataToLocalStorage);
 
-window.addEventListener('storage', e => {
-    items = JSON.parse(e.newValue);
-    list.dispatchEvent(new CustomEvent(itemsUpdated));
-});
+// window.addEventListener('storage', e => {
+//    items = JSON.parse(e.newValue);
+//    list.dispatchEvent(new CustomEvent('itemsUpdated'));
+// });
+
+bc.onmessage = e => {
+    items = e.data;
+    list.dispatchEvent(new CustomEvent('itemsUpdated'));
+}
 
 list.addEventListener('click', e => {
+    const id = e.target.id;
+
     if (e.target.matches('button')) {
-        deleteItem(e.target.value)
+        deleteItem(id)
     }
 
-    if (e.target.matches('input')) {
-        markAsCompleted(e.target.id)
+    if (e.target.matches('input[type="checkbox"]')) {
+        markAsCompleted(id);
     }
 });
